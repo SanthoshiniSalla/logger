d74f6bc5eb90fa9987cc462004aa2d93eacce830
SanthoshiniSalla <santhoshinisalla@gmail.com>
2021-11-16 12:34:00 +0530

    logger code

---
+++ librarycrud/api/routes/logger.js
@@ -0,0 +1,14 @@
+const {createLogger, format, transports } = require('winston');
+
+module.exports = createLogger({
+    transports:
+    new transports.File({
+        filename: 'logs/example.log',
+        format:format.combine(
+            format.timestamp({format: 'DD-MMM-YYYY HH:mm:ss'}),
+            format.align(),
+            format.printf(info =>`${info.level}:${[info.timestamp]}:${info.message}`)
+        )
+    }),
+    
+});
